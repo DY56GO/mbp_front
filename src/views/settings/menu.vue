a745035d3e17dd35c170bd0f351fe8b3091d120e
DY56 <2535368017@qq.com>
2023-06-01 18:11:11 +0800

    修改：修复了新增菜单表单太长，导致垂直不居中的情况。

--- src/views/settings/menu.vue
+++ src/views/settings/menu.vue
@@ -196,6 +196,25 @@
   </div>
 </template>
 
+<style scoped>
+::v-deep .el-dialog{
+       display: flex;
+       flex-direction: column;
+       margin:0 !important;
+       position:absolute;
+       top:50%;
+       left:50%;
+       transform:translate(-50%,-50%);
+       max-height:calc(100% - 30px);
+       max-width:calc(100% - 30px);
+   }
+  ::v-deep  .el-dialog .el-dialog__body{
+       flex:1;
+       overflow: auto;
+   }
+
+</style>
+
 <script>
 import { getMenuListPage, addMenu, updateMenu, deleteMenu } from '@/api/menu'
 
